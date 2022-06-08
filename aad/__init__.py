56f7f98c7f8079ce6b829764dc17862fa944625c
Adrien Renaudineau <adrien.renaudineau@universite-paris-saclay.fr>
2022-06-08 17:00:04 +0200

    [aad] Update accordingly to last commits

--- aad/__init__.py
+++ aad/__init__.py
@@ -1,8 +1,6 @@
 from ctypes.wintypes import WORD
 import aad.lld
 
-from bitarray.util import zeros as bitarray
-
 class AwesomeArrayDriver():
 	def __init__(self, pin = lld.LLDriver.DEFAULT_PID):
 		self._lld = lld.LLDriver(pin)
@@ -24,18 +22,17 @@ class AwesomeArrayDriver():
 		sr[lld.BL]  = ((bis == set) << col)
 		sr[lld.BLB] = ((bis != set) << col)
 
-		words_set = ['','','','',''] #* TMP For debug purpose
-
 		WORD_SIZE = 64
 		for bit_id in reversed(range(WORD_SIZE)):
 			for sr_id, sr_word in enumerate(sr):
 				self._lld.send_command('SET_SR', sr_id, (sr_word >> bit_id) & 1)
-				words_set[sr_id] += str((sr_word >> bit_id) & 1) 
 
 			# As all the sr share the same clk, we pulse after setting every individual inputs
-			self._lld.send_command('CLK')
+			self._lld.send_command('CLK', lld.CLK_ACK_NONE)
 
-		print(words_set)
+		# Reset every inputs afterward
+		for sr_id in range(len(sr)):
+				self._lld.send_command('SET_SR', sr_id, lld.RESET)
 
 	def set(self, col, row, bis=False):
 		self.__configure_sr(col, row, bis, set=True)
