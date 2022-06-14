d9979eecdf781f8e60da8e4778ebd2081f1d0d3c
Adrien Renaudineau <adrien.renaudineau@universite-paris-saclay.fr>
2022-06-14 13:53:10 +0200

    Update setup.py for easy install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='AwesomeArray-Driver',
+	version='0.0.1',
+	packages=find_packages(),
+	install_requires=[
+		'pyserial',
+		'pyvisa'
+	]
+)
