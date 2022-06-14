3370c8cd4754b90535cd03bd36902e2109ce52d9
Adrien Renaudineau <adrien.renaudineau@universite-paris-saclay.fr>
2022-06-14 22:21:17 +0200

    Do not include extlibs in install script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name='AwesomeArray-Driver',
 	version='0.0.1',
-	packages=find_packages(),
+	packages=['aad'],
 	install_requires=[
 		'pyserial',
 		'pyvisa'
